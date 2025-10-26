edbb6e2fa79543423267533dd0278c801dc8648f
JulioEstebanBeltranCardenas <L24170488@culiacan.tecnm.mx>
2025-10-26 06:00:51 +0000

    pila y cola

---
+++ pila/Pila.java
@@ -0,0 +1,61 @@
+package pila;
+
+public class Pila<T> {
+    private Nodo<T> cima;
+    private int tamanio;
+
+    public Pila() {
+        cima = null;
+        tamanio = 0;
+    }
+
+    public boolean esVacia() {
+        return cima == null;
+    }
+
+    public void apilar(T valor) {
+        Nodo<T> nuevo = new Nodo<>();
+        nuevo.setValor(valor);
+        nuevo.setSiguiente(cima);
+        cima = nuevo;
+        tamanio++;
+    }
+
+    public T retirar() {
+        if (esVacia()) {
+            return null;
+        }
+        
+        T valor = cima.getValor();
+        cima = cima.getSiguiente();
+        tamanio--;
+        return valor;
+    }
+
+    public T cima() {
+        if (esVacia()) {
+            return null;
+        }
+        return cima.getValor();
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void mostrar() {
+        if (esVacia()) {
+            System.out.println("Pila nula del pepe");
+            return;
+        }
+        
+        System.out.println("Pal lobby");
+        Nodo<T> actual = cima;
+        int contador = 1;
+        while (actual != null) {
+            System.out.println(contador + ". " + actual.getValor());
+            actual = actual.getSiguiente();
+            contador++;
+        }
+    }
+}
