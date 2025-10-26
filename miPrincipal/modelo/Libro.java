4f2b9a30e3d4fcb8aaae59c6ecac7c4a8c2fc356
JulioEstebanBeltranCardenas <L24170488@culiacan.tecnm.mx>
2025-10-26 05:47:52 +0000

    libro

--- miPrincipal/modelo/Libro.java
+++ miPrincipal/modelo/Libro.java
@@ -1,5 +1,43 @@
 package miPrincipal.modelo;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String codigo;
+
+    public Libro(String titulo, String autor, String codigo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return codigo;
+    }
+
+    public void setIsbn(String isbn) {
+        this.codigo = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro: " + titulo + " - Autor: " + autor + " - codigo: " + codigo;
+    }
     
 }
